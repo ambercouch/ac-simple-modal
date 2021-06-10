889d7ceabf4e573f781aea8549d3f2bcdb790121
Richard Arnold <richard@ambercouch.co.uk>
2021-06-10 10:26:02 +0100

    Add modal template

--- lib/acsm.php
+++ lib/acsm.php
@@ -1,1 +1,36 @@
 <?php
+
+function acsm_modal() {
+
+    $timber = false;
+
+    $template = __DIR__ . "/../templates/modal-template.php";
+    $output = "";
+
+
+    if($timber === false){
+
+        while (have_posts()):
+            the_post();
+            ob_start();
+            ?>
+            <?php include("$template"); ?>
+            <?php
+            $output .= ob_get_contents();
+            ob_end_clean();
+        endwhile;
+
+    }else{
+        $context = Timber::get_context();
+        $context['posts'] = Timber::get_posts();
+        $templates = array('loop-template.twig');
+        ob_start();
+        Timber::render( $templates, $context );
+        $output .= ob_get_contents();
+        ob_end_clean();
+    }
+
+    echo $output;
+
+}
+add_action( 'wp_footer', 'acsm_modal', 100 );
---
+++ templates/modal-template.php
@@ -0,0 +1,7 @@
+<?php
+
+?>
+
+<div class="c-modal-window" >
+    <p>This is just a modal window testing template directory</p>
+</div>
