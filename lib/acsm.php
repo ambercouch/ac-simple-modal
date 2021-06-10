bc5476166ff6507bfe970c1c90623d1ac5e4878b
Richard Arnold <richard@ambercouch.co.uk>
2021-06-10 12:51:25 +0100

    Add scripts and styles and enqueue

--- lib/acsm.php
+++ lib/acsm.php
@@ -34,3 +34,18 @@ function acsm_modal() {
 
 }
 add_action( 'wp_footer', 'acsm_modal', 100 );
+
+
+function acsm_enqueue_scripts(){
+
+    wp_register_script('acsm_modaal', plugins_url('../assets/js/scripts-acsm.js', __FILE__), array('jquery'), '1.1', true);
+    wp_enqueue_script('acsm_modaal');
+}
+
+add_action( 'wp_enqueue_scripts', 'acsm_enqueue_scripts' );
+
+function acsm_enqueue_styles() {
+    wp_register_style('acsm_modaal_styles', plugins_url('../assets/css/styles-acsm.css', __FILE__));
+    wp_enqueue_style('acsm_modaal_styles');
+}
+add_action( 'get_footer', 'acsm_enqueue_styles' );
