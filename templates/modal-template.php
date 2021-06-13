674566038bc213d6815b1886b96b465f55416fe7
Richard Arnold <richard@ambercouch.co.uk>
2021-06-13 16:36:36 +0100

    update styles and templates

--- templates/modal-template.php
+++ templates/modal-template.php
@@ -2,7 +2,37 @@
 
 ?>
 
-  <div id="covModal" data-modal="covPopup" style="display:none;">
-    covModal data modal
-  </div>
+  <div id="covModal" data-modal="covPopup" style="display:none;" class="c-acsm">
+<div class="c-acsm__content">
+    <h3 style="text-align: center;">We Are In Full Operation</h3>
+    <h3 style="text-align: center;">During February Lockdown</h3>
+    <p style="text-align: center;"><strong>Update 08 Feb 2021</strong></p>
+    <h4>Please read the following information</h4>
+
+    <p>We warmly welcome you back to Cathedral Dental Clinic.</p>
+    <p>We are delighted to inform you that during the latest lockdown we will continue to take care of your complete oral health needs. Your oral examination and planned or emergency treatment will continue to be provided with the highest level of care and attention that you are accustomed too.</p>
+    <p>There are detailed and stringent guidelines set by Welsh Deanery and HIW for every step of the process and operation, from the moment you contact us, to entering the clinic and until you leave.</p>
+    <p>Please read and adhere to the following information to keep yourself and our caring staff safe:</p>
+    <h3>POLICIES AND PROCEDURES WHEN ATTENDING APPOINTMENTS</h3>
+    <p>Strictly by appointment only. Please do not attend the clinic without an appointment, you can contact us by phone, email or via our website to make an appointment.</p>
+    <ul>
+      <li>Patients of all ages must wear a face mask. Please do not enter the clinic without a mask. If you do not have one, please ring the door bell and wait for assistance. We will supply you with a mask at a cost of £4.00.</li>
+      <li>Attend alone (unless chaperoning a dependent) and without any extra personnel belongings eg shopping bags etc.</li>
+      <li>Attend on time. do not attend more than 5 minutes early so as to avoid congestion in our waiting area and if over 5 minutes late I am afraid this will result in the cancellation of your appointment, please note a non-attendance fee will be applied in this instance.</li>
+      <li>Hand sanitise. Please use the hand sanitise station immediately when you enter the clinic and then report to reception where further advice will be provided.</li>
+      <li>Respect social distancing.</li>
+      <li>Please note to avoid cross contamination the patient toilets will not be available for use at this time.</li>
+    </ul>
+    <h3>Treatment</h3>
+    <p>Due to the pandemic any dental treatment that requires the use of a handpiece would be classified as AGP (Aerosol Generated Process). This requires surgery fallow time and deep decontamination after each treatment. Therefore it is imperative to plan ahead for treatment.</p>
+    <h3>Fees &amp; Payment</h3>
+    <p>Due to the high cost of PPE and materials as well the increased time required for treatment we have had to unavoidably temporarily increase our fee's. As always we will provide you with a detailed treatment plan and discuss with you all available options and relevant costs.</p>
+    <p> We are paying close attention to the daily Government guidance and instruction and we will inform you of any further updates as and when we receive them. Please register and visit our Facebook page for regular updates and latest information.</p>
+      <p>Thank you in advance for your ongoing support and we would like to wish you and your family a healthy year ahead.</p>
+
+    <p>Dr Vida Kolahi</p>
+    <p>Principal Dentist</p>
+    <p style="text-align: center;"><button class="sgpb-html-custom-button sgpb-html-custom-button-62827" style="width: 300px; height: 60px; border-width: 0px; border-radius: 2px; border-color: #650360; background-color: #650360; hover-color: #b281af; color: #ffffff; padding: 0; font-size: 22px; font-weight: 900;" data-options="%7B%22sgpb-custom-btn-title%22:%22Click%20to%20close%20%22,%22sgpb-custom-btn-width%22:%22300px%22,%22sgpb-custom-btn-height%22:%2260px%22,%22sgpb-custom-btn-border-width%22:%220px%22,%22sgpb-custom-btn-border-radius%22:%222px%22,%22sgpb-custom-btn-border-color%22:%22#650360%22,%22undefined%22:%22Cancel%22,%22sgpb-custom-btn-bg-color%22:%22#b281af%22,%22sgpb-custom-btn-text-color%22:%22#FFFFFF%22,%22sgpb-custom-button%22:%22hidePopup%22,%22sgpb-custom-button-copy-to-clipboard-text%22:%22some%20text%20here%22,%22sgpb-custom-button-copy-to-clipboard-message%22:%22Copied%20to%20clipboard!%22,%22sgpb-custom-button-redirect-URL%22:%22%22,%22sgpb-custom-button-popup%22:null%7D">Click to close </button></p>
+</div>
+</div>
 
