30c1a465b607ad51a844573212064ff0ad56916d
Richie Arnold <richard@ambercouch.co.uk>
2024-05-22 10:47:25 +0100

    ACSM - update then script to target specific modals

--- src/js/acsm-main.js
+++ src/js/acsm-main.js
@@ -1,3 +1,4 @@
+console.log('acsm testing may 2024 scope');
 let settings = {
     debug : true,
     expires : 1
@@ -17,58 +18,66 @@ function setCookie(name = 'acsm_closed', val = '1'){
 
 
 jQuery(function($) {
-$(function() {
-    $(document).ready(function () {
+    $(function() {
+        $(document).ready(function () {
 
-        $.each($('[data-modal-opener]'), function (i) {
+            // $.each($('[data-modal-opener]'), function (i) {
+            //
+            //     var openId = $(this).attr('data-modal-opener');
+            //
+            //
+            //     $(this).modaal({
+            //         content_source: '[data-modal="'+openId+'"]'
+            //     });
+            //
+            // })
 
-            var openId = $(this).attr('data-modal-opener');
+            // Event listener for mouse leaving the viewport
+            //         document.addEventListener('mouseleave', function(e) {
+            //             if (e.clientY <= 0 && Cookies.get('_acsm_intented') != 1) {
+            //                 //Cookies.set('_acsm_intented', '1')
+            //                 $('[data-modal-onexit]').modaal('open');
+            //             }
+            //         });
+
+            $('[data-modal-onload]').each(function(i) {
+                console.log('i : ' + i);
+
+                let $modal = $(this);
 
 
-            $(this).modaal({
-                content_source: '[data-modal="'+openId+'"]'
+                let modalSettings = {
+                    content_source: $modal,
+                    overlay_opacity: 0.4,
+                    after_close: (typeof settings !== 'undefined' && settings.debug != true) ? setCookie : function() { return false; },
+                    hide_close: false,
+                    after_open: function(){
+                        console.log('on open')
+                        let modalID = $(this).attr('data-modaal-scope');
+                        console.log('data-modaal-scope');
+                        console.log(modalID);
+                    },
+                    custom_class: 'act-modal-' + i // Set custom class to uniquely identify modals
+
+                };
+
+                $modal.modaal(modalSettings);
+
+                var acsm_closed = Cookies.get('acsm_closed');
+
+                // Open the onload modal if not closed
+                if (acsm_closed != 1) {
+                    $modal.modaal('open');
+                }
+
+                // Bind the close event to the modal instance
+                $(document).on('click', '.act-modal-' +i+ ' .modaal-close', function () {
+                    $modal.modaal('close');
+                })
             });
 
+
         })
-
-
-        let modalSettings = {
-            content_source: '[data-modal]',
-            overlay_opacity: 0.4
-        };
-
-
-
-        modalSettings.after_close = ( settings.debug != true ) ? setCookie : false;
-
-
-        $('[data-modal-onload]').modaal(modalSettings);
-        $('[data-modal-onexit]').modaal(modalSettings);
-
-        var acsm_closed = Cookies.get('acsm_closed')
-
-        // Open the onload modal if not closed
-        if(acsm_closed != 1 ){
-            $('[data-modal-onload]').modaal('open');
-        }
-
-// Event listener for mouse leaving the viewport
-        document.addEventListener('mouseleave', function(e) {
-            if (e.clientY <= 0 && Cookies.get('_acsm_intented') != 1) {
-                Cookies.set('_acsm_intented', '1')
-                $('[data-modal-onexit]').modaal('open');
-            }
-        });
-
-
-
-
-
-        $('[data-modal-close]').on('click', function () {
-            $('[data-modal]').modaal('close');
-        })
-
-    })
-});
+    });
 });
 
