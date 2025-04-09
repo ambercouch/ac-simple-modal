10200dc0b8a7a7d71e9d7dd13a89b56aa49e6966
Richie Arnold <richard@ambercouch.co.uk>
2025-04-09 11:27:18 +0100

    ACSM - Enhances acsm_modal shortcode with link type option
    Adds the ability to display the modal trigger as a button, text or image.
    
    Updates the modal template to handle the different link types, including styling and overlay button option for video modals.

--- lib/acsm.php
+++ lib/acsm.php
@@ -6,78 +6,70 @@ require_once(  'acsm-cpt.php' );
 require_once(  'acsm-acf.php' );
 
 
-if (!function_exists('acsm_modal_sc'))
-    {
+if (!function_exists('acsm_modal_sc')) {
+    function acsm_modal_sc($atts, $content = null) {
+        // Sanitize and set default attributes
+        $atts = shortcode_atts(array(
+            'modal_id'    => '',
+            'label'       => 'Show',
+            'class'       => 'c-btn',
+            'modal_type'  => 'inline',
+            'link_type'   => 'text',
+            'link_image'  => '',
+            'timber'      => 'false'
+        ), $atts, 'ac_simple_modal');
 
-        function acsm_modal_sc($atts, $content = null)
-        {
-            // Sanitize and set default attributes
-            $atts = shortcode_atts(array(
-                'modal_id' => '',
-                'label' => 'Show',
-                'class' => 'c-btn',
-                'modal_type' => 'inline',
-                'timber' => 'false'
-            ), $atts, 'ac_simple_modal');
-
-            // Return early if no modal ID is provided
-            if (empty($atts['modal_id']))
-            {
-                return '';
-            }
-
-            // Assign variables
-            $modal_id = esc_attr($atts['modal_id']);
-            $label = esc_html($atts['label']);
-            $class = esc_attr($atts['class']);
-            $modal_type = esc_attr($atts['modal_type']);
-            $timber = esc_attr($atts['timber']);
-
-            // Prepare output
-            $output = '';
-
-            // Create a new query for the modal post
-            $modal_query = new WP_Query(array(
-                'post_type' => 'acsm-simple-modal',
-                'p' => $modal_id,
-            ));
-
-            if ($modal_query->have_posts())
-            {
-                while ($modal_query->have_posts())
-                {
-                    $modal_query->the_post();
-
-                    ob_start();
-
-                    // Timber check (TIB site)
-                    if ($timber != 'false')
-                    {
-                        $output .= "timber";
-                        $output .= $timber;
-                        Timber::render('modal.twig', array(
-                            'modal_id' => $modal_id,
-                            'label' => $label,
-                            'class' => $class,
-                            'modal_type' => $modal_type,
-                            'post' => Timber::get_post(),
-                        ));
-                    } else
-                    {
-                        // Fallback for PCM or non-Timber installs
-                        include(__DIR__ . "/../templates/modal-template-on-click.php");
-                    }
-
-                    $output .= ob_get_clean();
-                }
-            }
-
-            wp_reset_postdata();
-            return $output;
+        if (empty($atts['modal_id'])) {
+            return '';
         }
 
-        add_shortcode('ac_simple_modal', 'acsm_modal_sc');
+        // Assign variables
+        $modal_id   = esc_attr($atts['modal_id']);
+        $label      = esc_html($atts['label']);
+        $class      = esc_attr($atts['class']);
+        $modal_type = esc_attr($atts['modal_type']);
+        $link_type  = esc_attr($atts['link_type']);
+        $link_image = esc_attr($atts['link_image']);
+        $timber     = esc_attr($atts['timber']);
+
+        $output = '';
+
+        $modal_query = new WP_Query(array(
+            'post_type' => 'acsm-simple-modal',
+            'p'         => $modal_id,
+        ));
+
+        if ($modal_query->have_posts()) {
+            while ($modal_query->have_posts()) {
+                $modal_query->the_post();
+
+                ob_start();
+
+                if ($timber !== 'false') {
+                    Timber::render('modal.twig', array(
+                        'modal_id'   => $modal_id,
+                        'label'      => $label,
+                        'class'      => $class,
+                        'modal_type' => $modal_type,
+                        'link_type'  => $link_type,
+                        'link_image' => $link_image,
+                        'post'       => Timber::get_post(),
+                    ));
+                } else {
+                    include(__DIR__ . "/../templates/modal-template-on-click.php");
+                }
+
+                $output .= ob_get_clean();
+            }
+        }
+
+        wp_reset_postdata();
+        return $output;
     }
+
+    add_shortcode('ac_simple_modal', 'acsm_modal_sc');
+}
+
 
 
 if (!function_exists('acsm_modal')) {
--- templates/modal-template-on-click.php
+++ templates/modal-template-on-click.php
@@ -1,9 +1,34 @@
-<a href="#modal<?php echo esc_attr(get_the_ID()); ?>" class="c-acsm__link <?php echo esc_attr($class); ?>" data-modal-type="<?php echo esc_attr($modal_type); ?>" data-modal-opener="modal<?php echo esc_attr(get_the_ID()); ?>">
-    <?php echo esc_html($label); ?>
-</a>
+<?php if ($link_type === 'button') : ?>
+  <div class="is-link-type-btn c-acsm-btn <?php echo esc_attr($class); ?>">
+    <a href="#modal<?php echo esc_attr(get_the_ID()); ?>" class="c-acsm-btn__link" data-modal-type="<?php echo esc_attr($modal_type); ?>" data-modal-opener="modal<?php echo esc_attr(get_the_ID()); ?>">
+      <span class="c-acsm-btn__label"><?php echo esc_html($label); ?></span>
+    </a>
+  </div>
 
-<div id="modal<?php echo esc_attr(get_the_ID()); ?>" data-modal="modal<?php echo esc_attr(get_the_ID()); ?>" style="display:none;" class="c-acsm">
-  <div class="c-acsm__content c-acsm__content-on-click">
+<?php elseif ($link_type === 'text') : ?>
+  <a href="#modal<?php echo esc_attr(get_the_ID()); ?>" class="is-link-type-text <?php echo esc_attr($class); ?>" data-modal-type="<?php echo esc_attr($modal_type); ?>" data-modal-opener="modal<?php echo esc_attr(get_the_ID()); ?>">
+      <?php echo esc_html($label); ?>
+  </a>
+
+<?php elseif ($link_type === 'image' && $link_image) : ?>
+  <a href="#modal<?php echo esc_attr(get_the_ID()); ?>" class="is-link-type-image <?php echo esc_attr($class); ?>" data-modal-type="<?php echo esc_attr($modal_type); ?>" data-modal-opener="modal<?php echo esc_attr(get_the_ID()); ?>">
+    <div class="c-acsm-img-wrapper <?php echo ($modal_type === 'video') ? 'has-overlay-button' : 'has-label-below'; ?>">
+        <?php echo wp_get_attachment_image($link_image, 'full'); ?>
+
+        <?php if (!empty($label)) : ?>
+            <?php if ($modal_type === 'video') : ?>
+            <span class="c-acsm-img__button is-overlay"><?php echo esc_html($label); ?></span>
+            <?php else : ?>
+            <div class="c-acsm-img__label"><?php echo esc_html($label); ?></div>
+            <?php endif; ?>
+        <?php endif; ?>
+    </div>
+  </a>
+<?php endif; ?>
+
+
+<div id="modal<?php echo esc_attr(get_the_ID()); ?>" data-modal="modal<?php echo esc_attr(get_the_ID()); ?>" style="display:none;" class="c-acsm__modal is-on-click">
+  <div class="c-acsm__content">
       <?php the_content(); ?>
   </div>
 </div>
